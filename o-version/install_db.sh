418561dc7e8d89434a559b577bef904a523a409d
Andrii Nikitin <andrii.nikitin@mariadb.com>
2017-07-10 08:53:49 +0200

    making sure test database present by default

--- o-version/install_db.sh
+++ o-version/install_db.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-__workdir/../_depot/o-tar/__version/*/mysql_install_db --defaults-file=__workdir/my.cnf --datadir=__workdir/dt --user=$(whoami) --basedir=__workdir/../_depot/o-tar/__version
+__workdir/../_depot/o-tar/__version/*/mysql_install_db --defaults-file=__workdir/my.cnf --datadir=__workdir/dt --user=$(whoami) --basedir=__workdir/../_depot/o-tar/__version && mkdir -p __workdir/dt/test
+
