d51598b36f90484246ce5e397405b41465853ef7
Andrii Nikitin <andrii.nikitin@mariadb.com>
2017-09-07 18:16:07 +0200

    handling extra parameters in gen_cnf

--- o-version/gen_cnf.sh
+++ o-version/gen_cnf.sh
@@ -13,15 +13,38 @@ socket=__datadir/my.sock
 skip-grant-tables
 
 server_id=__wid
-lc_messages_dir=__workdir/../_depot/o-tar/__version/share
 port=__port
 socket=__datadir/my.sock
 datadir=__datadir
 log-error=__datadir/error.log
 
+!include __workdir/mysqldextra.cnf
+EOL
+
+cat > __workdir/mysqldextra.cnf <<EOL
+[mysqld]
+lc_messages_dir=__workdir/../_depot/o-tar/__version/share
 pid_file=__datadir/p.id
 plugin-dir=__workdir/../_depot/o-tar/__version/lib/plugin
 EOL
+
+
+[ ! -z "$1" ] && for o in $@ ; do
+  option_name=${o%%=*}
+  option_value=${o#*=}
+
+  if [ -f __workdir/"$option_name".sh ] ; then
+    if [ "$option_value" == 1 ] ; then
+      mkdir -p __workdir/config_load
+      cp __workdir/"$option_name".sh  __workdir/config_load/
+    elif [ "$option_value" == 0 ] ; then
+      rm -f __workdir/config_load/$option_name.sh
+    fi
+  else
+    echo $o >> __workdir/mysqldextra.cnf
+  fi
+done
+
 
 [ -d __workdir/config_load ] && for config_script in __workdir/config_load/*
 do
