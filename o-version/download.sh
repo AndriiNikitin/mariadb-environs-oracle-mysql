c2249ef7deb7450fedda944235a12574b72cbf22
Andrii Nikitin <andrii.nikitin@mariadb.com>
2017-09-21 10:34:53 +0200

    add support for 5.7.19+

--- o-version/download.sh
+++ o-version/download.sh
@@ -2,10 +2,13 @@
 
 # example url  https://dev.mysql.com/get/Downloads/MySQL-5.7/mysql-5.7.18-linux-glibc2.5-x86_64.tar.gz 
 
+# 5.7.19 and above should have glibc2.12 in url
+define(ver5719plus,regexp(__version,`5.7.\(19\|[2-9].\)',yes))
+define(glibcver,ifelse(ver5719plus,yes,12,5))
 ver=__version
 major=${ver%\.*}
 
-file=https://dev.mysql.com/get/Downloads/MySQL-${major}/mysql-${ver}-linux-glibc2.5-x86_64.tar.gz
+file=https://dev.mysql.com/get/Downloads/MySQL-${major}/mysql-${ver}-linux-glibc2.glibcver-x86_64.tar.gz
 
 mkdir -p __workdir/../_depot/o-tar/__version
 
